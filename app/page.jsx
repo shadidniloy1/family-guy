1c000139ae6e8dfbfbfde8f992aa57a028570981
Shadid Hasan <shadidhasan1@gmail.com>
2025-09-21 15:30:23 +0600

    gigidy

--- app/page.jsx
+++ app/page.jsx
@@ -10,10 +10,39 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { endpoint } from '@/utils/endpoint'
 
+async function getAllCharacters(){
+  const data = await fetch(`${endpoint}/characters`)
+
+  if(!data.ok){
+    throw new Error('Failed to fetch data')
+  }
+
+  return data.json();
+}
+
 export default async function Page() {
+  const data = await getAllCharacters();
   return (
-    <main className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl">hello from boilerplate!</h1>
+    <main>
+      <Container className="grid grid-cols-2 gap-1 py-5 md:grid-cols-3 lg:grid-cols-4">
+        {data?.characters?.map(item =>{
+          return (
+            <Link
+              href={`/characters/${item.slug}`}
+              key={item.name}
+              className="overflow-hidden rounded-md"
+            >
+              <Image
+                src={item.avatar}
+                alt=""
+                className="transition-all duration-500 hover:scale-110 hover:-rotate-2"
+                width={500}
+                height={500}
+              />
+            </Link>
+          )
+        })}
+      </Container>
     </main>
   )
 }
